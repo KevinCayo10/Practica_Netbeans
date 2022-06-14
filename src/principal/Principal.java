526bdd036e05750411ab6a392361cefbd6e9f2f2
KevinCayo10 <kcayo6155@uta.edu.ec>
2022-06-13 21:42:06 -0500

    "Se añadio mi nombre Kevin Cayo"

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -16,7 +16,6 @@ public class Principal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        System.out.println("Kevin Cayo");
     }
-    
 }
